6a8a89ad68e4823c06b3dfe5c72382ce8fba8647
Raphael Lima <raphaellimag@gmail.com>
2021-04-30 12:43:36 -0300

    refactor: remove uuidv4

---
+++ src/database/migrations/1619720600229-CreateUsers.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUsers1619720600229 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'lastname',
+            type: 'varchar'
+          },
+          {
+            name: 'nickname',
+            type: 'varchar(30)',
+            isUnique: true
+          },
+          {
+            name: 'adress',
+            type: 'varchar'
+          },
+          {
+            name: 'bio',
+            type: 'varchar(100)',
+            isNullable: true
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updateAt',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
